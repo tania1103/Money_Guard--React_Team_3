6bb21881cba75bf01423fd7bf443e2e6d309564f
Tatiana Culeac <culeactatiana1103@gmail.com>
2025-03-28 17:00:57 +0200

    2 commit

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,89 @@
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { Icons } from "../Icons/Icons";
+
+import { loginThunk } from "../../redux/auth/operations";
+import { validationSchemaLogin } from "../../helpers/loginSchema";
+import s from "./LoginForm.module.css";
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const onSubmit = ({ email, password }, { resetForm }) => {
+    dispatch(loginThunk({ email, password }));
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={validationSchemaLogin}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form className={s.form}>
+          <div className={s.modalEllipse}></div>
+          <div className={s.loginLogo}>
+            <Icons
+              name={"logo"}
+              width={27}
+              height={26}
+              className={s.iconLogo}
+            />
+            <p className={s.loginTitle}>Money Guard</p>
+          </div>
+          <label className={s.label}>
+            <div className="inputContainerLogo">
+              <Icons
+                name={"email"}
+                width={20}
+                height={16}
+                className={s.iconName}
+              />
+              <Field
+                className={s.loginInput}
+                type="email"
+                name="email"
+                placeholder="E-mail"
+              />
+              <ErrorMessage name="email" component="div" className={s.error} />
+            </div>
+          </label>
+          <label className={s.label}>
+            <div className="inputContainerLogo">
+              <Icons
+                name={"password"}
+                width={17}
+                height={17}
+                className={s.iconName}
+              />
+              <Field
+                className={s.loginInput}
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={s.error}
+              />
+            </div>
+          </label>
+          <button className={s.but} disabled={isSubmitting} type="submit">
+            Login
+          </button>
+          <Link className={s.link} to="/register">
+            Register
+          </Link>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
---
+++ src/redux/auth/operations.js
@@ -0,0 +1,92 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import { toasterCustomStyles } from "../../helpers/toasterCustomStyles";
+
+axios.defaults.baseURL = "https://wallet.b.goit.study";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = "";
+};
+
+export const registerThunk = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post("/api/auth/sign-up", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      toast.error(
+        "This email is already registered. Please use a different email or log in!",
+        toasterCustomStyles
+      );
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginThunk = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post("/api/auth/sign-in", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      toast.error(
+        "Incorrect username or password. Please check your credentials and try again!",
+        toasterCustomStyles
+      );
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logoutThunk = createAsyncThunk(
+  "auth/logout",
+  async (_, thunkAPI) => {
+    try {
+      await axios.delete("/api/auth/sign-out");
+      clearAuthHeader();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const refreshUserThunk = createAsyncThunk(
+  "auth/refresh",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (!persistedToken) {
+      return thunkAPI.rejectWithValue("Unable to fetch user");
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const { data } = await axios.get("/api/users/current");
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getBalanceThunk = createAsyncThunk(
+  "auth/getBalance",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get("/api/users/current");
+      return data.balance;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
