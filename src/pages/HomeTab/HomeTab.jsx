f63fa9b79cfdacf8d847cc73243413d69810d35c
DutuGabriel <gdutu94@gmail.com>
2025-04-01 14:34:51 +0300

    Added functionality for the dashboard,header and navbar

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,1 +1,29 @@
 //GABI
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+import AddButton from '../../components/AddButton/AddButton';
+import Balance from '../../components/Balance/Balance';
+import TransactionsList from '../../components/TransactionsList/TransactionsList';
+
+import { getTransactions } from '../../redux/transaction/operations';
+import { useMedia } from '../../hooks/useMedia';
+
+const HomeTab = () => {
+  const { isMobile } = useMedia();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTransactions());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {isMobile && <Balance />}
+      <TransactionsList />
+      <AddButton />
+    </div>
+  );
+};
+
+export default HomeTab;
