07e2d02b62ed3123ab1aef2b279bcc0016b2cb9f
Alexandra Stavila <109295061+AlexandraMdv@users.noreply.github.com>
2025-04-01 09:40:10 +0300

    Alexandra

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,1 +1,22 @@
 //Alexandra
+import { useDispatch } from 'react-redux';
+import { openAddModal } from '../../redux/modal/slice';
+import { Icons } from '../Icons/Icons';
+import s from './AddButton.module.css';
+
+const AddButton = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <button
+      className={s.addBtn}
+      onClick={() => {
+        dispatch(openAddModal());
+      }}
+    >
+      <Icons className={s.plusIcon} name={'plus'} width={20} height={20} />
+    </button>
+  );
+};
+
+export default AddButton;
