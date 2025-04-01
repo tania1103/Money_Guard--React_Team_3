57ffc96e3fe6c28b9ed43f494a08c95c6eaa9a5b
Alexandra Stavila <109295061+AlexandraMdv@users.noreply.github.com>
2025-04-01 09:40:10 +0300

    Alexandra

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,1 +1,57 @@
 //ALEXANDRA
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import s from './Chart.module.css';
+import { useSelector } from 'react-redux';
+import { selectPeriodTransactions } from '../../redux/transaction/selectors';
+import {
+  backgroundColor,
+  hoverBackgroundColor,
+  options,
+} from '../../helpers/statisticsColors';
+import statisticsNoTransactions from '../../images/noTransactions/statisticsNoTransactions.webp';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const Chart = () => {
+  const { categoriesSummary = [], periodTotal = 0 } = useSelector(
+    selectPeriodTransactions
+  );
+  const categoriesNames = categoriesSummary.filter(
+    category => category.type === 'EXPENSE'
+  );
+  if (categoriesNames.length === 0) {
+    return (
+      <div className={s.noTransactionsContainer}>
+        <img
+          src={statisticsNoTransactions}
+          alt="No Transactions Yet"
+          className={s.noTransactionsImage}
+        />
+      </div>
+    );
+  }
+
+  const data = {
+    labels: categoriesNames.map(category => category.name),
+    datasets: [
+      {
+        data: categoriesNames.map(category => category.total),
+        backgroundColor,
+        borderWidth: 0,
+        hoverBackgroundColor,
+      },
+    ],
+  };
+
+  return (
+    <div className={s.doughnutContainer}>
+      <Doughnut data={data} options={options} />
+      <div className={s.doughnutCenter}>
+        <span className={s.innerCenter}> {periodTotal.toFixed(2)} RON</span>
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
--- src/components/ModalEditTransaction/ModalEditTransaction.jsx
+++ src/components/ModalEditTransaction/ModalEditTransaction.jsx
@@ -1,1 +1,17 @@
 //ALEXANDRA
+import { useSelector } from 'react-redux';
+
+import ModalWrapper from '../ModalWrapper/ModalWrapper';
+import EditTransactionForm from '../EditTransactionForm/EditTransactionForm';
+
+import { selectIsEditModalOpen } from '../../redux/modal/selectors';
+
+const ModalEditTransaction = () => {
+  const isEditModalOpen = useSelector(selectIsEditModalOpen);
+  return (
+    <ModalWrapper isOpenModal={isEditModalOpen}>
+      <EditTransactionForm />
+    </ModalWrapper>
+  );
+};
+export default ModalEditTransaction;
